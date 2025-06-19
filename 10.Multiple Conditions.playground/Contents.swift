9781717ad62ea2178620b3317029e0a76260b6c6
Sanket Ughade <sanketughade96@gmail.com>
2025-06-19 22:17:58 +0530

    Completed the multiple conditions part

---
+++ 10.Multiple Conditions.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+
+let a = true
+let b = false
+
+//Multiple conditions can be added using ELSE-IF blocks
+
+if a {
+    print("Code to run if a is true")
+} else if b {
+    print("Code to run if a is false but b is true")
+} else {
+    print("Code to run if both a and b are false")
+}
+
+//Multiple conditions can be added using logical operators(&&, ||, etc)
+let temp = 25
+
+if temp > 20 && temp < 30 {
+    print("It's a nice day.")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, escooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path...")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
