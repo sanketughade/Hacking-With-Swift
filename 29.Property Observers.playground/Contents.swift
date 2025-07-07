dfdf7999b7e497017c173ed01f32cb43e79565b0
Sanket Ughade <sanketughade96@gmail.com>
2025-07-07 10:48:44 +0530

    Completed the Property Observers section

---
+++ 29.Property Observers.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+//We can execute some code if we want when the property value of a struct changes
+//We use willSet and didSet for this as shown in the below examples
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+        
+        didSet {
+            print("There are now \(contacts.count) contacts")
+            print("Old value was: \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
